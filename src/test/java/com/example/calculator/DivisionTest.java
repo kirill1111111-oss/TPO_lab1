97482d5adfe06dae0eb74c2852aef2914c19d574
Кирилл <kiril.j2018@yandex.ru>
2025-05-27 19:48:24 +0300

    12

--- src/test/java/com/example/calculator/DivisionTest.java
+++ src/test/java/com/example/calculator/DivisionTest.java
@@ -1,8 +1,6 @@
 package com.example.calculator;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class DivisionTest {
